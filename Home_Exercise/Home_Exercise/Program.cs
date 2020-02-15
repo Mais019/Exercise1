01543add54b0bc54cadd4508f0578fc26fa9f84d
Mais <mimi2000@live.dk>
2020-02-15 20:20:54 +0100

    Changed the code

--- Home_Exercise/Home_Exercise/Program.cs
+++ Home_Exercise/Home_Exercise/Program.cs
@@ -8,7 +8,35 @@ namespace Exercise1
 	Write a C# Sharp program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum.
 	*/
 
+
         static void Main(string[] args)
+        {
+            Console.WriteLine("Write first number"); int Inta;
+
+
+            Inta = int.Parse(Console.ReadLine());
+
+            int IntSum = 0;
+
+            Console.WriteLine("Write second number"); int Intb;
+            Intb= int.Parse(Console.ReadLine());
+
+            if (Inta != Intb){IntSum = Inta + Intb;}
+            else {
+                if (Inta == Intb)
+                {
+                    int _int = Inta + Intb;
+                    IntSum = _int * 3;
+                }
+            }
+
+            Console.WriteLine("Result is " + IntSum);
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+
+        }
+
+       /* static void Main(string[] args)
         {
             Console.WriteLine(Sum(5,5));
             Console.WriteLine(Sum(12, 10));
@@ -29,8 +57,7 @@ namespace Exercise1
             }
 
 
-        }
-
+        }*/
 
     }
 }
