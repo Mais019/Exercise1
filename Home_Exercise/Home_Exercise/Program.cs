90a1b82a546d4d95c3aeafcca67a97d2cdb63162
Mais <mimi2000@live.dk>
2020-02-15 11:25:59 +0100

    Added the exercise

---
+++ Home_Exercise/Home_Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(2, 2));
+            Console.WriteLine(Sum(12, 10));
+            Console.WriteLine(Sum(-5, 2));
+        }
+
+        static int Sum(int a, int b)
+        {
+            if (a == b)
+            {
+                return a + b;
+            }
+            else
+            {
+                return (a + b) * 3;
+            }
+
+
+        }
+    }
+}
+
