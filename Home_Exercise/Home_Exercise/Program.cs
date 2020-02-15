2f8ef75b504515dab144ba0e5125449da86c01f9
Mais <mimi2000@live.dk>
2020-02-15 14:54:58 +0100

    ..

--- Home_Exercise/Home_Exercise/Program.cs
+++ Home_Exercise/Home_Exercise/Program.cs
@@ -4,11 +4,17 @@ namespace Exercise1
 {
     class Program
     {
+        /*
+	Write a C# Sharp program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum.
+	*/
+
         static void Main(string[] args)
         {
-            Console.WriteLine(Sum(2, 2));
+            Console.WriteLine(Sum(5,5));
             Console.WriteLine(Sum(12, 10));
             Console.WriteLine(Sum(-5, 2));
+            Console.ReadLine();
+
         }
 
         static int Sum(int a, int b)
@@ -24,6 +30,8 @@ namespace Exercise1
 
 
         }
+
+
     }
 }
 
